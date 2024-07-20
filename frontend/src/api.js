5a255f127c19d7e2e13303fc8039b5c156dcefdb
Hari <hariharan256633@gmail.com>
2024-07-20 10:14:12 +0530

    Initial commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,23 +1,7 @@
-import axios from "axios";
-import { ACCESS_TOKEN } from "./constants";
+import axios from 'axios';
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem(ACCESS_TOKEN);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-export default api;
+export default axiosInstance;
